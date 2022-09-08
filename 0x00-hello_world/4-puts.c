4be1e49e94b1de146d51bc34d9654bfc7ace34d8
etoyin <toyinadesina60@gmail.com>
2022-09-08 08:33:03 -0700

    Task 4: correct3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * 
+ *
+ *    * Description: 'the program's description'
+ *
+ *     * 
+ *
+ *      * Return: Always 0 (Success)
+ *
+ **/
+
 int main(void)
 {
 	char myOutPut[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(myOutPut);
 
 	return (0);
