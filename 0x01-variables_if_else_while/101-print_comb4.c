a00be15c806f289a6fdee518d8d300b6d5f3878a
etoyin <toyinadesina60@gmail.com>
2022-09-09 15:29:57 -0700

    Task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Description: hjhjjjjjfj
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j, k;
+
+	while (i < 10)
+	{
+		j = 0;
+
+		while (j < 10)
+		{
+			k = 0;
+
+			while (k < 10)
+			{
+				if (i < j && j < k && i != j && j != k && i != k)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar('0' + k);
+
+					if (i + j + K != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
