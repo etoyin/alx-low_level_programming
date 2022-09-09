73c39cf27f921993f87080e46fcec2ed6772ce1a
etoyin <toyinadesina60@gmail.com>
2022-09-09 12:31:36 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: main block
+ * Return: 0
+ */
+
+int main (void)
+{
+	for (int i = 'a'; i <= 'z'; i++)
+		putchar(i);
+		putchar("\n");
+	return (0);
+}
