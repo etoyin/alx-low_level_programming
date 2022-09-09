68a482fd1fca57c771f0e74e85363d5f558619ab
etoyin <toyinadesina60@gmail.com>
2022-09-09 13:45:11 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: print all single digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(\'\n');
+	return (0);
+}
