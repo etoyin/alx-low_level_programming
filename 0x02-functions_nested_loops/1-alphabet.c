c0f17c00499b11ecdefc2799ae7a6b43b7c5c42e
etoyin <toyinadesina60@gmail.com>
2022-09-14 07:40:16 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,32 @@
+#include "main.h";
+
+/**
+ * main -main block
+ * Description: main block
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet
+ * Description: a function to print all lower aplphabet
+ * Return: string
+ */
+
+void print_alphabet(void)
+{
+	char i = "a";
+
+	while (i <= "z")
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
