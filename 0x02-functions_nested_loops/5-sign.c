aae1aad2dfb46c2ea37391acda383a0880b0f7c8
etoyin <toyinadesina60@gmail.com>
2022-09-14 08:51:51 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - to print sign of a number
+ * @c: input parameter
+ * Return 0 or 1
+ */
+
+int print_sign(int c)
+{
+	int res = 0;
+	char sign;
+
+	if (c > 0)
+	{
+		sign = '+';
+		res = 1;
+	}
+	if (c == 0)
+	{
+		sign = '0';
+		res = 0;
+	}
+	if (c < 0)
+	{
+		sign = '-';
+		res = -1;
+	}
+	_putchar(sign);
+	return (res);
+}
