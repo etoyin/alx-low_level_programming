b3ce7cd3bc4ad4ec957451ab1a537a0295e1bea5
etoyin <toyinadesina60@gmail.com>
2022-09-14 15:12:59 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - 9 times table
+ * Description: as above
+ */
+
+void times_table(void)
+{
+	int i = 0;
+	int j;
+
+	while (i <= 9)
+	{
+		j = 0;
+
+		while (j <= 9)
+		{
+			_putchar((i * j) + 48);
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			j++
+		}
+		_putchar('\n');
+		i++;
+	}
+}
