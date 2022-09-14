7ba4e71ec841c920e89802f4c9256f1ec4ae97dc
etoyin <toyinadesina60@gmail.com>
2022-09-14 16:20:52 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@
 void times_table(void)
 {
 	int i = 0;
-	int j, prod;
+	int j, prod, next_prod1, next_prod2;
 
 	while (i <= 9)
 	{
@@ -17,6 +17,8 @@ void times_table(void)
 		while (j <= 9)
 		{
 			prod = i * j;
+			next_prod1 = (i * (j + 1));
+			netx_prod2 = ((i + 1) * j);
 
 			if (prod >= 10)
 				_putchar((prod / 10) + 48);
@@ -26,7 +28,7 @@ void times_table(void)
 			if (j < 9)
 			{
 				_putchar(',');
-				if (prod < 9)
+				if ((next_prod1 < 9) || (next_prod2 < 9))
 					_putchar(' ');
 				_putchar(' ');
 			}
