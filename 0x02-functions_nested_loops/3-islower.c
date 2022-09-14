764db3a1e3934df400a94b459def7557cb9c375c
etoyin <toyinadesina60@gmail.com>
2022-09-14 08:22:53 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,11 +9,11 @@ int _islower(int c)
 {
 	int res = 0;
 
-	if (c >= 65 || c <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		res = 0;
 	}
-	else if (c >= 97 || c <= 122)
+	else if (c >= 97 && c <= 122)
 	{
 		res = 1;
 	}
