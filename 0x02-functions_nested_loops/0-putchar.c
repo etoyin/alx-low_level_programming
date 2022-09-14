f8f11384ef9347c7f8710bd8ba18379da1efed40
etoyin <toyinadesina60@gmail.com>
2022-09-14 01:56:54 -0700

    Task 1 correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - main block
@@ -8,6 +8,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	char str[] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar("\n");
 	return (0);
 }
