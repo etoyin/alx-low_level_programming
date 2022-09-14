b4d236116c1ea631efae7d76655a79e87f15e1c3
etoyin <toyinadesina60@gmail.com>
2022-09-14 08:31:52 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha -  to return all alphabet to be true
+ * @c - parameter
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
