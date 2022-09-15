1d6d5c9f8c987bd30a6f589acb8cbf3325ce0308
etoyin <toyinadesina60@gmail.com>
2022-09-14 17:39:50 -0700

    Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,20 +29,18 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((prod % 10) + 48);
-				}
+				}				}
 				else if (prod <= 99 && prod > 9)
 				{
 					_putchar(' ');
 					_putchar((prod / 10) + 48);
-					_putchar((prod % 10) + 48);
 				}
 				else
 				{
 					_putchar((prod / 100) + 48);
 					_putchar(((prod / 10) % 10) + 48);
-					_putchar((prod % 100) + 48);
 				}
+				_putchar((prod % 10) + 48);
 				j++;
 			}
 			i++;
