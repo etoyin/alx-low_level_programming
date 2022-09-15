94610ac0f5b4cad597371ecff7c145966ca0a3cd
etoyin <toyinadesina60@gmail.com>
2022-09-14 17:30:53 -0700

    Task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * times_table
+ * @n: input
+ */
+
+void print_times_table(int n)
+{
+	int i = 0;
+	int j;
+
+	if (n > 0 && n <= 15)
+	{
+		while (i <= n)
+		{
+			j = 1;
+
+			_putchar(48);
+
+			while (j <= n)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = i * j;
+
+				if (prod <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((prod % 10) + 48);
+				}
+				else if (prod <= 99 && prod > 9)
+				{
+					_putchar(' ');
+					_putchar((prod / 10) + 48);
+					_putchar((prod % 10) + 48);
+				}
+				else
+				{
+					_putchar((prod / 100) + 48);
+					_putchar(((prod / 10) % 10) + 48);
+					_putchar((prod % 100) + 48);
+				}
+				j++;
+			}
+			i++;
+			_putchar('\n');
+		}
+	}
+}
