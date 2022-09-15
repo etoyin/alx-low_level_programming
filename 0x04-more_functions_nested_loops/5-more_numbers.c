2bcd915b0709eb94ad1e48cbf8d75239f253e9a4
etoyin <toyinadesina60@gmail.com>
2022-09-15 13:29:56 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - more numbers
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int j;
+
+	while (i <= 10)
+	{
+		j = 0;
+
+		while (j <= 14)
+		{
+			_putchar(j + 48);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
