6379b0df19ff550d74028c3489cb94d4c0d62b39
etoyin <toyinadesina60@gmail.com>
2022-09-15 05:49:19 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - main block
+ * Description: Test the code
+ * Return: 0 or 1
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 20 % 2;
+	postive_negative(i);
+	return (0);
+}
