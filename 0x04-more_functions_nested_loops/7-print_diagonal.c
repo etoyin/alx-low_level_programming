c8392b69b25c55acdebc43cb496a98002d6d425e
etoyin <toyinadesina60@gmail.com>
2022-09-15 13:57:56 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print
+ * @n: is the number
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int posn, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (posn = 1; posn <= n ++posn)
+		{
+			for (space = 1; space <= posn; ++space)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
