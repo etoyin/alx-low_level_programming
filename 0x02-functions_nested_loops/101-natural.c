0abcf57deb4ff83fcb700e2f8dda258b581120c0
etoyin <toyinadesina60@gmail.com>
2022-09-14 17:58:39 -0700

    Task 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * naturals - natural numbers
+ * Description: print naturals
+ */
+
+void naturals(void)
+{
+	int i = 0;
+	int c = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			c = c + i;
+		}
+		i++;
+	}
+	printf("%d\n", c);
+}
