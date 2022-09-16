29f5e14f5fbecfd4960ccf1b230f6853acb3c9e5
etoyin <toyinadesina60@gmail.com>
2022-09-16 05:22:49 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_fizz_buzz -  print fizz buzz
+ * Return: void
+ */
+
+void print_fizz_buzz(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+}
