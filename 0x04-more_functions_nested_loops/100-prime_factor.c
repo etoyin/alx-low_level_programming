233e3261e5f50ba84c02f4e002c17f1a8b1a4d01
etoyin <toyinadesina60@gmail.com>
2022-09-16 06:59:51 -0700

    Task 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: block block
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int i = 3, n = 612852475143;
+
+	while (i < sqrt(n))
+	{
+		while (n % i == 0 && n != i)
+			n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
