2b009a56e9b7df28e11a29e5fa6e096c6bd67bc1
etoyin <toyinadesina60@gmail.com>
2022-09-16 05:29:13 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int main(void)
 
 	while (i <= 100)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
@@ -25,4 +25,5 @@ int main(void)
 			printf(" ");
 	}
 	printf("\n");
+	return (0);
 }
