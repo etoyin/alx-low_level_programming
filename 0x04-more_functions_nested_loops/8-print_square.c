18f1f9090c929044f54c84cd6ae8a9d843b5d788
etoyin <toyinadesina60@gmail.com>
2022-09-16 03:54:42 -0700

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - function to print square
+ * @size: size input
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i = 0;
+	int j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (i < size)
+		{
+			j = 0;
+
+			while (j < size)
+			{
+				_putchar(35);
+				j++;
+			}
+			i++;
+		}
+	}
+}
