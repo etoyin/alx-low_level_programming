503f44f251f7bbf73fe7ba44c4c01dd2aac0ab31
etoyin <toyinadesina60@gmail.com>
2022-09-19 12:58:13 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - string
+ * @s: input
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+	char temp;
+
+	while (s[j] != '\0')
+		j++;
+
+	while (i < j / 2)
+	{
+		temp = s[i];
+		s[i] = s[j - 1 - i];
+		s[j - 1 - i] = temp;
+	}
+}
