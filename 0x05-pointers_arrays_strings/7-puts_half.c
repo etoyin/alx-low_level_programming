2161dc3a7c812950938c1a6c570ca634a6312fe7
etoyin <toyinadesina60@gmail.com>
2022-09-19 22:57:41 +0100

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - function
+ * @str: input
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, l = 0;
+
+	while (str[l] != '\0')
+		l++;
+
+	if (l % 2 != 0)
+		i = (l - 1) / 2;
+	else
+		i = l / 2;
+
+	while (str[i] != '\0')		
+		_putchar(str[i]);
+	_putchar('\n');
+}
