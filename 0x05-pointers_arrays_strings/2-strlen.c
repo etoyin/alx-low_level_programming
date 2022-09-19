438733d8c1489d04cd782024306d961c66ca2001
etoyin <toyinadesina60@gmail.com>
2022-09-19 11:11:37 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * strlen -strlen block
+ * Description: string length function
+ * @s: input param
+ * Return: 0 or 1 or any integer
+ */
+
+void strlen(char *s)
+{
+	int i = 0, counter = 0;
+
+	while (*s != '\0')
+	{
+		s[++i];
+		counter++;
+	}
+	return (counter)
+}
