7d647c8a8111a3ac61e79b06fd981e92b372c2fe
etoyin <toyinadesina60@gmail.com>
2022-09-19 07:47:21 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - a function to swap integers
+ * @a: input a
+ * @b: input b
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = &a;
+	*a = &b;
+	*b = temp;
+}
