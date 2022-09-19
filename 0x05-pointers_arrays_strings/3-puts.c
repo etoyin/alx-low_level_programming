a1609e8fb0de0e8f6933d8c4a0e31cd84dce63e4
etoyin <toyinadesina60@gmail.com>
2022-09-19 11:37:47 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - function to print a string 
+ * @str: input param
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
