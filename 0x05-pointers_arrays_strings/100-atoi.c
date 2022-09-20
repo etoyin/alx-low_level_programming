c38e19bb776d8506a8b5a0d3d8892edc907780ab
etoyin <toyinadesina60@gmail.com>
2022-09-20 07:21:55 -0700

    Task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,7 +4,8 @@
  * _atoi - changes string to number
  * @s: input string
  * Return: number
- */int _atoi(char *s)
+ */
+int _atoi(char *s)
 {
 	int i = 1;
 	unsigned int num = 0;
@@ -16,8 +17,7 @@
 			num = num * 10 + (*s - '0');
 		else if (num > 0)
 			break;
-	}
-	while (*s++);
+	} while (*s++);
 	return (num * i);
 
 }
