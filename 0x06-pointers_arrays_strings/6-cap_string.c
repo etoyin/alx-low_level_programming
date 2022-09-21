e285835a24efdabf35abd44d463a3dfd0dde606c
etoyin <toyinadesina60@gmail.com>
2022-09-21 20:49:41 +0100

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize string
+ * @s: input
+ * Return: void
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		switch (s[i])
+		{
+		case ' ':
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+			break;
+		case '\t':
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+			break;
+		case '\n':
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+			break;
+		case ',':
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+			break;
+		case ';':
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+			break;
+		case '.':
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+			break;
+		case '!':
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+			break;
+		case '?':
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+			break;
+		case '"':
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+			break;
+		case '(':
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+			break;
+		case ')':
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+			break;
+		case '{':
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+			break;
+		case '}':
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+			break;
+		default:
+			break;
+		}
+		i++;
+	}
+}
