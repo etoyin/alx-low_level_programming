beb765e73a80330dd2c50875fcfc0883dfe70eb1
etoyin <toyinadesina60@gmail.com>
2022-09-22 21:46:11 +0100

    Task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: modified s.
+ */
+
+char *rot13(char *s)
+{
+	int i = 0, j;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (*(s + i) == alpha[j])
+			{
+				*(s + i) = rot13[j];
+				break;
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
