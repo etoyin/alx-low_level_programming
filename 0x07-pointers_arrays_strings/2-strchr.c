2cc8a38ba472b53ede5fc0ed2aa6fdc69b53dabe
etoyin <toyinadesina60@gmail.com>
2022-09-26 22:53:29 +0100

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,11 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
-		if (s[i] == c)
+		if (*s == c)
 			return (s);
-		i++;
+		s++;
 	}
 	if(*s == c)
 	{
