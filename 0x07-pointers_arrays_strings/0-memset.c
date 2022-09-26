0c9b1968c92b3f29098939ea7ea852c047ef7158
etoyin <toyinadesina60@gmail.com>
2022-09-26 21:18:49 +0100

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - works like the memset() function
+ * @s: first input
+ * @b: second input
+ * @n: third input
+ * 
+ * Return: a pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	char *st = s;
+
+	while (i < n)
+	{
+		s[i++] = b;
+	}
+	return (st);
+}
