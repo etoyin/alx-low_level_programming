bd44759c9976ba5d58110a9e1c10a2ee9468cbcf
etoyin <toyinadesina60@gmail.com>
2022-09-26 21:28:39 +0100

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,12 +11,10 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
-	char *st = s;
 
 	while (i < n)
 	{
-		s[i] = b;
-		i++;
+		s[i++] = b;
 	}
-	return (st);
+	return (s);
 }
