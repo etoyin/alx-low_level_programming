18036d4a0987be8d230bc0816ee39b2ca4f8e09a
etoyin <toyinadesina60@gmail.com>
2022-09-26 23:28:11 +0100

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * @s: input
+ * @accept: input
+ * Return: numbr of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j, k = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				k = 1;
+				break;
+			}
+			j++;
+		}
+
+		if (k == 0)
+		{
+			break;
+		}
+	}
+	return (i);
+}
