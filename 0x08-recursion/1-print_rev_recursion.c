bec09411ece79c997055da30370d3d32187d575f
unknown <toyinadesina60@gmail.com>
2022-09-28 13:16:07 +0100

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,13 +8,20 @@
 
 void _print_rev_recursion(char *s)
 {
+	char *e;
+	int index;
+
 	if (*s == '\0')
 	{
 		return;
 	}
 	_print_rev_recursion(++s);
     _putchar(*s);
-    if (s == s[0])
+
+	e = strchr(s, s[0]);
+	index = (int)(e - s);
+
+    if (index == 0)
     {
         _putchar('\n');
     }
