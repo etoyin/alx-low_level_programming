c425ad0f18ddf253edbebb04cbfb51c0ae75d720
etoyin <toyinadesina60@gmail.com>
2022-09-29 23:22:56 +0100

    Task 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,10 +8,10 @@
 
 int is_palindrome(char *s)
 {
-	int counter = 0, start = 0;
+	int last = 0, start = 0;
 
-	counter = _strlen_recursion(s);
-	return (_palindrome(s, start, counter));
+	last = _strlen_recursion(s) - 1;
+	return (_palindrome(s, start, last));
 
 }
 
@@ -23,13 +23,17 @@ int is_palindrome(char *s)
  * Return: 0 or 1
  */
 
-int _palindrome(char *s, int start, int length)
+int _palindrome(char *s, int start, int last)
 {
-	if (start < (length - 1))
+	// if (start >= last)
+	// {
+	// 	return (1);
+	// }
+	if (start < last)
 	{
-		if (s[start] != s[length - 1])
+		if (s[start] != s[last])
 			return (0);
-		return (_palindrome(s, start + 1, length - 2));
+		return (_palindrome(s, start + 1, last - 1));
 	}
 	return (1);
 }
