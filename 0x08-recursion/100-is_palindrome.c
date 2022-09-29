7c0c8ee3dcd5dbc8810705bae0b6a357d45db630
etoyin <toyinadesina60@gmail.com>
2022-09-29 23:09:10 +0100

    Task 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -33,3 +33,18 @@ int _palindrome(char *s, int start, int length)
 	}
 	return (1);
 }
+
+/**
+ * _strlen_recursion - get the length of string
+ * @s: input string
+ * Return: integer length of string;
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
