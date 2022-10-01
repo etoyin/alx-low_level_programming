8b54a434327225237b3918579172eb25d051052a
etoyin <toyinadesina60@gmail.com>
2022-10-01 20:29:55 +0100

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- a program that prints its name
+ *
+ * @argc: input holds the number of arguments passed
+ * @argv: input array pointer that holds the arguments passed
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i <= argc)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
