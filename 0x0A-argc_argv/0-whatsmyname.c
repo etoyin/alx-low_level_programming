14d4433f80ef4e50b6cd69330a6ffd2e4faa7d92
etoyin <toyinadesina60@gmail.com>
2022-10-01 20:12:12 +0100

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- a program that prints its name
+ *
+ * @argc: input holds the number of arguments passed
+ * @argv: input array pointer that holds the arguments passed
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
