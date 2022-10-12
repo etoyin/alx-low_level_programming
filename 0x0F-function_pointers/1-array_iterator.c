93501b085f1dfe5805236263ee4b3014c98f0916
etoyin <toyinadesina60@gmail.com>
2022-10-12 19:30:52 +0100

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,6 @@
 #include "function_pointers.h"
 
 /**
- * gain mastery over a mistery and you get a ministry
  * array_iterator - a function that executes a given function
  *                  as a parameter on each element of an array
  * @array: array to iterate
