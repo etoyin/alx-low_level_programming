9b83d29d381f7dfb1f63463566007e5c8e714f07
etoyin <toyinadesina60@gmail.com>
2022-10-13 22:54:26 +0100

    Task 1

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,5 @@
 #include "variadic_functions.h"
-
+#include <stdarg.h>
 /**
  * print_strings - prints strings.
  * @separator: string to be printed between the strings.
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,6 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
 
-#include <stdarg.h>
 #include <stdio.h>
 
 
