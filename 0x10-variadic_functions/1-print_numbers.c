1e685238eb229e8e8e6be0e71e51549a4907d676
etoyin <toyinadesina60@gmail.com>
2022-10-13 22:52:40 +0100

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
-
-	printf("\n");
 	va_end(valist);
+    printf("\n");
 }
