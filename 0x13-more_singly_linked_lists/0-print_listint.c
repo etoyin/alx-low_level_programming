bec368844eca87cfb293741bf52191a432c58e8b
unknown <toyinadesina60@gmail.com>
2022-10-25 09:40:01 +0100

    Task 0 first commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - print integer in a linked list
+ * @h: pointer to head first node
+ * Return: Number of nodes
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t node_count = 1;
+
+	if (h == NULL)
+		return (0);
+	while (h->next != NULL)
+	{
+		if (h->n == NULL)
+			printf("%d\n", 0);
+		else
+			printf("%d\n", h->n);
+
+		h = h->next;
+		node_count += 1;
+	}
+	printf("%d\n", h->n);
+	return (node_count);
+}
