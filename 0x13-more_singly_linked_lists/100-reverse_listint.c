23753bc0ea01690b2638c5e7252603488b493c97
unknown <toyinadesina60@gmail.com>
2022-10-26 19:37:24 +0100

    Task 100

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -24,3 +24,30 @@ listint_t *reverse_listint(listint_t **head)
 	*head = temp2;
 	return (*head);
 }
+
+#include "lists.h"
+
+/**
+ * add_nodeint - return the length of the elements
+ * @head: pointer to head first node
+ * @n: element to be added
+ * Return: Number of nodes
+ **/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *temp;
+
+	temp = malloc(sizeof(listint_t));
+
+	if (temp == NULL)
+		return (NULL);
+
+	if (*head == NULL)
+		temp->next = NULL;
+	else
+		temp->next = *head;
+	temp->n = n;
+	*head = temp;
+	return (*head);
+}
