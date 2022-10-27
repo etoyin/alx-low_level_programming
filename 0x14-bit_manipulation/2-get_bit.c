3fed946769d6a3f2b910e03ec059d1f8d059474b
unknown <toyinadesina60@gmail.com>
2022-10-27 15:39:07 +0100

    Task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,9 +10,10 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > 63)
+		return (-1);
 	if (index < 1)
 		return (n & 1);
-	else
-		return (get_bit(n >> 1, index - 1));
-	return (-1);
+
+	return (get_bit(n >> 1, index - 1));
 }
