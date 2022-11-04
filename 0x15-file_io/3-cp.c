9538adc86ba38e861c9e4f830b01517d57a4dab8
unknown <toyinadesina60@gmail.com>
2022-11-04 19:22:26 +0100

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,16 +9,13 @@
 char *create_buffer(char *file)
 {
 	char *buffer;
-
 	buffer = malloc(sizeof(char) * 1024);
-
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
-
 	return (buffer);
 }
 
@@ -29,9 +26,7 @@ char *create_buffer(char *file)
 void close_file(int fd)
 {
 	int c;
-
 	c = close(fd);
-
 	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
@@ -43,9 +38,7 @@ void close_file(int fd)
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
- *
  * Return: 0 on success.
- *
  * Description: If the argument count is incorrect - exit code 97.
  *              If file_from does not exist or cannot be read - exit code 98.
  *              If file_to cannot be created or written to - exit code 99.
@@ -55,18 +48,15 @@ int main(int argc, char *argv[])
 {
 	int from, to, r, w;
 	char *buffer;
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 	do {
 		if (from == -1 || r == -1)
 		{
@@ -75,7 +65,6 @@ int main(int argc, char *argv[])
 			free(buffer);
 			exit(98);
 		}
-
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
@@ -87,12 +76,9 @@ int main(int argc, char *argv[])
 
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
-
 	} while (r > 0);
-
 	free(buffer);
 	close_file(from);
 	close_file(to);
-
 	return (0);
 }
