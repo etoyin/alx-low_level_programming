ecc2e54043a3af8233267418cee0bc0d04315bab
unknown <toyinadesina60@gmail.com>
2022-12-08 14:56:39 +0100

    lists.h

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,21 +9,16 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-
 	int counter = 0;
-
 	if (h == NULL)
 		return (counter);
-
 	while (h->prev != NULL)
 		h = h->prev;
-
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		counter++;
 	}
-
 	return (counter);
 }
